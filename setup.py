84064d0d0b1dbbcaaa5eef1dd8477d5bbdc3bff2
hsercanatli <hsercanatli@gmail.com>
2016-01-04 16:34:28 +0200

    added pip setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='makamtonicidentifier',
+      version='0.1',
+      description='An Automatic Tonic Identification Method for Turkish Makam Music Recordings',
+      author='Hasan Sercan Atli',
+      url='https://github.com/hsercanatli/tonic_identifier',
+      packages=['tonic_identifier'],
+)
