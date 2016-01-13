86f1e66e3885221e04118d9172a38413400c67b7
hsercanatli <hsercanatli@gmail.com>
2016-01-13 17:46:40 +0200

    Refactoring the histogram function

--- tonic_identifier/histogram.py
+++ tonic_identifier/histogram.py
@@ -6,27 +6,18 @@ from numpy import histogram
 from pypeaks import Data
 
 class Histogram(Data):
-    def __init__(self, post_filter=True, freq_limit=False, bottom_limit=64, upper_limit=1024):
+    def __init__(self, times=1, bottom_freq_limit=64, upper_freq_limit=1024,
+                 chunk_limit=60, bottom_limit=0.965, upper_limit=1.035):
 
         # inputs
-        self.post_filter = post_filter
-        self.freq_limit = freq_limit
+        self.times = times
+        self.bottom_freq_limit = bottom_freq_limit
+        self.upper_freq_limit = upper_freq_limit
+        self.chunk_limit = chunk_limit
         self.bottom_limit = bottom_limit
         self.upper_limit = upper_limit
 
-    @staticmethod
-    def energy_filter(pitch, threshold=0.002):
-        """
-        checks the saliences
-        """
-        for element in pitch:
-            if element[2] <= threshold and element[1] != 0:
-                element[1] = 0
-                element[2] = 0
-
-        return pitch
-
-    def decompose_into_chunks(self, pitch, bottom_limit=0.8, upper_limit=1.2):
+    def decompose_into_chunks(self, pitch):
         """
         decomposes the given pitch track into the chunks.
         """
@@ -47,7 +38,7 @@ class Histogram(Data):
             # non-zero chunks
             else:
                 interval = float(pitch[i + 1][1]) / float(pitch[i][1])
-                if bottom_limit < interval < upper_limit:
+                if self.bottom_limit < interval < self.upper_limit:
                     temp_pitch.append(pitch[i])
                 else:
                     temp_pitch.append(pitch[i])
@@ -55,12 +46,9 @@ class Histogram(Data):
                     temp_pitch = []
         pitch_chunks.append(temp_pitch)
 
-        if self.post_filter:
-            return self.post_filter_chunks(pitch_chunks, chunk_limit=60, freq_limit=self.freq_limit)
-        else:
-            return pitch_chunks
+        return self.post_filter_chunks(pitch_chunks)
 
-    def post_filter_chunks(self, pitch_chunks, chunk_limit=60, freq_limit=True):
+    def post_filter_chunks(self, pitch_chunks):
         """
         Postfilter for the pitchChunks
         deletes the zero chunks
@@ -71,14 +59,13 @@ class Histogram(Data):
         pitch_chunks = delete(pitch_chunks, zero_chunks)
 
         # deleting small Chunks
-        small_chunks = [i for i in range(0, len(pitch_chunks)) if len(pitch_chunks[i]) <= chunk_limit]
+        small_chunks = [i for i in range(0, len(pitch_chunks)) if len(pitch_chunks[i]) <= self.chunk_limit]
         pitch_chunks = delete(pitch_chunks, small_chunks)
 
         # frequency limit
-        if freq_limit:
-            limit_chunks = [i for i in range(0, len(pitch_chunks)) if pitch_chunks[i][0][1] >= self.upper_limit or
-                            pitch_chunks[i][0][1] <= self.bottom_limit]
-            pitch_chunks = delete(pitch_chunks, limit_chunks)
+        limit_chunks = [i for i in range(0, len(pitch_chunks)) if pitch_chunks[i][0][1] >= self.upper_freq_limit or
+                        pitch_chunks[i][0][1] <= self.bottom_freq_limit]
+        pitch_chunks = delete(pitch_chunks, limit_chunks)
 
         return pitch_chunks
 
@@ -89,17 +76,14 @@ class Histogram(Data):
         """
         return [pitch_chunks[i][j] for i in range(len(pitch_chunks)) for j in range(len(pitch_chunks[i]))]
 
-    def compute(self, pitch, times=1):
+    def compute(self, pitch):
         """
         Computes the histogram for given pitch track
         """
-        pitch = self.energy_filter(pitch)
-
         pitch_chunks = self.decompose_into_chunks(pitch)
         pitch = self.recompose_chunks(pitch_chunks)
 
-        pitch_chunks = self.decompose_into_chunks(pitch, bottom_limit=0.965, upper_limit=1.035)
-        pitch = self.recompose_chunks(pitch_chunks)
+        print len(pitch_chunks)
 
         pitch = [sample[1] for sample in pitch]
 
@@ -114,8 +98,9 @@ class Histogram(Data):
             print '!!!PROBLEM!!!\nCheck the given audio recording. Range is lower than 4 octave'
 
         # 1/3 holderian comma in 4 octave
-        step_no = 4 * 53 * 3 * times
-        edges = (arange(step_no + 1) * (1. / (3 * times * 53.))) + min_logf0
+        step_no = 4 * 53 * 3 * self.times
+        edges = (arange(step_no + 1) * (1. / (3 * self.times * 53.))) + min_logf0
+
         # pitch histogram
         hist = histogram(log_pitch, edges)[0]
 
