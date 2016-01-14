5d84852ba9a0e17e0dd93b84a2c3f148060338ef
Sertan Senturk <contact@sertansenturk.com>
2016-01-14 19:36:11 +0100

    Changed histogram class with Pitch distribution

--- tonic_identifier/tonic_identifier.py
+++ tonic_identifier/tonic_identifier.py
@@ -3,15 +3,25 @@ __author__ = 'hsercanatli'
 
 from numpy import median
 from numpy import where
-from histogram import Histogram
+import numpy as np
+import PitchDistribution
+from pitchfilter.pitchfilter import PitchPostFilter
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 
 class TonicLastNote:
-    def __init__(self):
-        self.stable_pitches = []
-        self.time_interval = None
+    def __init__(self, kernel_width=7.5, step_size=7.5, min_freq=64, max_freq=1024, 
+                 lower_interval_thres=0.7, upper_interval_thres=1.3, min_chunk_size=60):
+
+        self.kernel_width = kernel_width  # cents, kernel width of the pitch distribution, ~1/3 Holderian comma
+        self.step_size  = step_size  # cents, step size of the bins of the pitch distribution, ~1/3 Holderian comma
+
+        self.min_freq = min_freq  # minimum frequency allowed
+        self.max_freq = max_freq  # maximum frequency allowed
+        self.lower_interval_thres = lower_interval_thres  # the smallest value the interval can stay before a new chunk is formed
+        self.upper_interval_thres = upper_interval_thres  # the highest value the interval can stay before a new chunk is formed
+        self.min_chunk_size = min_chunk_size  # minimum number of samples to form a chunk
 
     @staticmethod
     def find_nearest(array, value):
@@ -21,26 +31,43 @@ class TonicLastNote:
 
     def identify(self, pitch, plot=False, verbose=False):
         """
-        plot function
+        Identify the tonic by detecting the last note and extracting the frequency
         """
         tonic = 0
         octave_wrapped = 1
 
-        # getting histograms 3 times more resolution
-        histo = Histogram(times=3)
-        pitch_chunks, normal_histo = histo.compute(pitch)
+        # convert Hz to cent using a dummy value for distribution computation
+        dummyFreq = 440.0
+        pitch = np.array(pitch)
+        tempCentVals = PitchDistribution.hz_to_cent(pitch[:,1], dummyFreq)
 
-        self.stable_pitches = histo.peaks["peaks"][0]
+        # compute the pitch distribution and distribution peaks
+        distribution = PitchDistribution.generate_pd(tempCentVals, ref_freq=dummyFreq, 
+            kernel_width=self.kernel_width, step_size=self.step_size)
+        distribution.bins = PitchDistribution.cent_to_hz(distribution.bins, 
+            dummyFreq)
+
+        # get the stable pitches
+        peaks = distribution.detect_peaks()
+        peakId = peaks[0]
+        stable_pitches = distribution.bins[peakId]
+
+        # get pitch chunks
+        flt = PitchPostFilter(lower_interval_thres=self.lower_interval_thres, 
+                              upper_interval_thres=self.upper_interval_thres,
+                              min_freq=self.min_freq, max_freq=self.max_freq)
+        pitch_chunks = flt.decompose_into_chunks(pitch)
+        pitch_chunks = flt.post_filter_chunks(pitch_chunks)
 
         cnt = 1
         while tonic is 0:
             last_chunk = [element[1] for element in pitch_chunks[-cnt]]
 
             last_note = median(last_chunk)
-            self.stable_pitches = sorted(self.stable_pitches, key=lambda x: abs(last_note - x))
+            stable_pitches = sorted(stable_pitches, key=lambda x: abs(last_note - x))
 
-            for j in range(len(self.stable_pitches)):
-                tonic_candidate = float(self.stable_pitches[j])
+            for j in range(len(stable_pitches)):
+                tonic_candidate = float(stable_pitches[j])
 
                 if (tonic_candidate / (2 ** (2. / 53))) <= last_note <= (tonic_candidate * (2 ** (2. / 53))):
                     tonic = {"estimated_tonic": tonic_candidate,
@@ -72,28 +99,31 @@ class TonicLastNote:
 
         # octave correction
         temp_tonic = tonic['estimated_tonic'] / 2
-        temp_candidate = self.find_nearest(self.stable_pitches, temp_tonic)
-        temp_candidate_ind = [i for i, x in enumerate(histo.peaks['peaks'][0]) if x == temp_candidate]
-        temp_candidate_occurrence = histo.peaks["peaks"][1][temp_candidate_ind[0]]
+        temp_candidate = self.find_nearest(stable_pitches, temp_tonic)
+        temp_candidate_ind = [i for i, x in enumerate(stable_pitches) if x == temp_candidate]
+        temp_candidate_occurrence = peaks[1][temp_candidate_ind[0]]
 
-        tonic_ind = [i for i, x in enumerate(histo.peaks['peaks'][0]) if x == tonic['estimated_tonic']]
-        tonic_occurrence = histo.peaks["peaks"][1][tonic_ind[0]]
+        tonic_ind = [i for i, x in enumerate(stable_pitches) if x == tonic['estimated_tonic']]
+        tonic_occurrence = peaks[1][tonic_ind[0]]
 
         if (temp_candidate / (2 ** (1. / 53))) <= temp_tonic <= (temp_candidate * (2 ** (1. / 53))):
             if tonic['estimated_tonic'] >= 400:
-                print "OCTAVE CORRECTED!!!!"
+                if verbose:
+                    print "OCTAVE CORRECTED!!!!"
                 octave_wrapped = 0
                 tonic = {"estimated_tonic": temp_candidate,
                          "time_interval": [pitch_chunks[-cnt][0][0], pitch_chunks[-cnt][-1][0]]}
 
             if tonic_occurrence <= temp_candidate_occurrence:
-                print "OCTAVE CORRECTED!!!!"
+                if verbose:
+                    print "OCTAVE CORRECTED!!!!"
                 octave_wrapped = 0
                 tonic = {"estimated_tonic": temp_candidate,
                          "time_interval": [pitch_chunks[-cnt][0][0], pitch_chunks[-cnt][-1][0]]}
 
         else:
-            print "No octave correction!!!"
+            if verbose:
+                print "No octave correction!!!"
             octave_wrapped = 1
 
         return_tonic = {
@@ -107,35 +137,40 @@ class TonicLastNote:
                         }
 
         if plot:
-            self.plot(pitch, return_tonic, pitch_chunks, histo)
+            self.plot(pitch, return_tonic, pitch_chunks, distribution)
 
         if verbose:
             print last_note
             print tonic
-            print sorted(self.stable_pitches)
+            print sorted(stable_pitches)
 
-        return return_tonic, pitch, pitch_chunks, histo
+        return return_tonic, pitch, pitch_chunks, distribution, stable_pitches
 
-    def plot(self, pitch, tonic, pitch_chunks, histo):
+    def plot(self, pitch, tonic, pitch_chunks, distribution):
         fig, (ax1, ax2, ax3) = plt.subplots(3, num=None, figsize=(18, 8), dpi=80)
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0, hspace=0.4)
 
         # plot title
-        ax1.set_title('Recording Histogram')
+        ax1.set_title('Pitch Distribution')
         ax1.set_xlabel('Frequency (Hz)')
         ax1.set_ylabel('Frequency of occurrence')
+
         # log scaling the x axis
         ax1.set_xscale('log', basex=2, nonposx='clip')
         ax1.xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%d'))
-        # recording histogram
-        ax1.plot(histo.x, histo.y, label='SongHist', ls='-', c='b', lw='1.5')
+
+        # recording distribution
+        ax1.plot(distribution.bins, distribution.vals, label='SongHist', ls='-', c='b', lw='1.5')
+
         # peaks
-        ax1.plot(histo.peaks['peaks'][0], histo.peaks['peaks'][1], 'cD', ms=6, c='r')
+        peaks = distribution.detect_peaks()
+        ax1.plot(peaks[0], peaks[1], 'cD', ms=6, c='r')
+
         # tonic
         ax1.plot(tonic['value'],
-                 histo.y[where(histo.x == tonic['value'])[0]], 'cD', ms=10)
+                 distribution.vals[where(distribution.bins == tonic['value'])[0]], 'cD', ms=10)
 
-        # pitch histogram
+        # pitch distributiongram
         ax2.plot([element[0] for element in pitch], [element[1] for element in pitch], ls='-', c='r', lw='0.8')
         ax2.vlines([element[0][0] for element in pitch_chunks], 0,
                    max([element[1]] for element in pitch))
