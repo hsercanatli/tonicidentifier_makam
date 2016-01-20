4f6c204eb685260bc4b8bbfcdd1c069fd0760d2a
hsercanatli <hsercanatli@gmail.com>
2016-01-20 23:55:30 +0200

    package name is corrected

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 from setuptools import setup
@@ -8,5 +7,5 @@ setup(name='makamtonicidentifier',
       description='An Automatic Tonic Identification Method for Turkish Makam Music Recordings',
       author='Hasan Sercan Atli',
       url='https://github.com/hsercanatli/tonic_identifier',
-      packages=['tonic_identifier'], requires=['numpy', 'pypeaks']
+      packages=['tonicidentifier'], requires=['numpy', 'pypeaks']
       )
